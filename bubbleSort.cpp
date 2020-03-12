e068e252334fa8e9ab61716d528dce5585e89415
suraj60081 <sk9718680309@gmail.com>
2020-03-12 11:41:01 +0530

    bubble sort iteratively

---
+++ bubbleSort.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[]={8,-1,4,10,11};
+    int n=5;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1]) 
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<5;i++){
+        cout<<a[i]<<" ";
+    }
+   
+    return 0;
+}
+
